10d2f4d39f7bc0c0be0fa210d85a187c62f57f16
CaioMGT <florianita100.mendes@gmail.com>
2023-01-30 20:55:05 -0300

    WIP tablist thing

--- src/main/java/com/caiomgt/sabotage/Sabotage.java
+++ src/main/java/com/caiomgt/sabotage/Sabotage.java
@@ -2,6 +2,12 @@ package com.caiomgt.sabotage;
 
 import com.caiomgt.sabotage.events.PlayerChat;
 import com.caiomgt.sabotage.events.PlayerJoin;
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
 import org.bukkit.Server;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.PluginManager;
@@ -12,13 +18,24 @@ public final class Sabotage extends JavaPlugin {
     ConsoleCommandSender console = server.getConsoleSender();
     PluginManager manager = server.getPluginManager();
     public teams teams = new teams();
+    ProtocolManager protocolManager;
     @Override
     public void onEnable() {
         // Plugin startup logic
         teams.create();
         console.sendMessage("[SABOTAGE] Starting Sabotage");
+        // Register Events
         manager.registerEvents(new PlayerJoin(), this);
         manager.registerEvents(new PlayerChat(this, teams), this);
+
+        // Manage Tablist Packet
+        protocolManager = ProtocolLibrary.getProtocolManager();
+        protocolManager.addPacketListener(new PacketAdapter(this, ListenerPriority.NORMAL, PacketType.Play.Server.PLAYER_LIST_HEADER_FOOTER) {
+            @Override
+            public void onPacketReceiving(PacketEvent event){
+                console.sendMessage("Received packet: " + event.getPacket().getStringArrays().toString());
+            }
+        });
     }
 
     @Override
