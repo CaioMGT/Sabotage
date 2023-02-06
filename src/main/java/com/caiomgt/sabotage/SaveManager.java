4fba4ab78ad9ac2f01b624b69e365c8eb0fce65b
CaioMGT <florianita100.mendes@gmail.com>
2023-02-06 17:36:40 -0300

    Begin save manager

--- src/main/java/com/caiomgt/sabotage/SaveManager.java
+++ src/main/java/com/caiomgt/sabotage/SaveManager.java
@@ -1,20 +1,39 @@
 package com.caiomgt.sabotage;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonWriter;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.UUID;
 
 
 public class SaveManager {
     Plugin plugin;
+    Gson gson = new Gson();
+
     public SaveManager(Plugin plugin) {
         this.plugin = plugin;
     }
-    public void save(UUID uuid) {
-
+    public boolean save(UUID uuid, Object data) {
+        try {
+            gson.toJson(data, new FileWriter(plugin.getDataFolder().getPath() + "\\" + uuid));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
-    public void load(UUID uuid) {
-
+    public void addKarma(UUID uuid, int Karma) {
+        //setKarma(uuid, );
+    }
+    public Object load(UUID uuid) {
+        return gson.fromJson(plugin.getDataFolder().getPath() + "\\" + uuid, Object.class);
     }
 }
