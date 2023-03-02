48a393c75f93f0c996ac7a343cbe90ae067a8d5f
CaioMGT <caio@caiomgt.com>
2023-03-01 22:44:51 -0300

    Begin work on conversion (not functioning)

--- src/main/java/com/caiomgt/sabotage/events/PlayerChat.java
+++ src/main/java/com/caiomgt/sabotage/events/PlayerChat.java
@@ -2,6 +2,10 @@ package com.caiomgt.sabotage.events;
 
 import com.caiomgt.sabotage.GameManager;
 import com.caiomgt.sabotage.teams;
+import io.papermc.paper.chat.ChatRenderer;
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.Component;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
@@ -10,6 +14,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scoreboard.Team;
+import org.jetbrains.annotations.NotNull;
 
 
 import java.util.Set;
@@ -25,10 +30,22 @@ public class PlayerChat implements Listener {
         this.manager = manager;
     }
     @EventHandler
-    public void onChat(AsyncPlayerChatEvent event) {
+    public void onChat(AsyncChatEvent event) {
         // I'll leave this for a different pr, requires some restructuring to transition to components
         Player plr = event.getPlayer();
         Team plrTeam = plr.getScoreboard().getEntityTeam(plr);
+        event.renderer(new ChatRenderer() {
+            @Override
+            public @NotNull Component render(@NotNull Player player, @NotNull Component component, @NotNull Component message, @NotNull Audience audience) {
+                if (audience.getClass() == Player.class) {
+                    Player audiencePlr = (Player) audience;
+                    if (audiencePlr.getScoreboard().getEntityTeam(audiencePlr) == Teams.sabs) {
+                        event.message().
+                    }
+                }
+                return message;
+            }
+        });
         if (manager.gameStarted && !manager.gracePeriod){
             if (!(plrTeam == null) && plrTeam.equals(Teams.dets)) {
                 event.setFormat(ChatColor.BLUE + "<%s> " + ChatColor.RESET + "%s");
